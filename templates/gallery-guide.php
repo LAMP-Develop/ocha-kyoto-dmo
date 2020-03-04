f240049d6f60e9c047231ce46510f4d63d6dd7cf
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-04 17:00:49 +0900

    update

---
+++ templates/gallery-guide.php
@@ -0,0 +1,45 @@
+<?php
+$home = esc_url(home_url());
+$wp_url = get_template_directory_uri();
+?>
+<div class="gallery-guide bg-gray sec mb-4">
+<div class="wrap">
+<h2 class="txt-c">User guide</h2>
+<ol>
+<li>
+<div class="step">
+<span class="crimson">01</span>
+<p>Choose</p>
+</div>
+<div class="inner">
+<div class="img">
+</div>
+<p>Search images and click the desired from the list.</p>
+</div>
+</li>
+<li>
+<div class="step">
+<span class="crimson">02</span>
+<p>Insert</p>
+</div>
+<div class="inner">
+<div class="img">
+</div>
+<p>Fill out the form and click Download.</p>
+</div>
+</li>
+<li>
+<div class="step">
+<span class="crimson">03</span>
+<p>Download</p>
+</div>
+<div class="inner">
+<div class="img">
+</div>
+<p>Download complete screen or Download image by e-mail.</p>
+</div>
+</li>
+</ol>
+<p class="txt-r mt-1"><a href="#" target="_blank"><small>Kyoto Tea Country Gallery Terms of Use ＞</small></a></p>
+</div>
+</div>
