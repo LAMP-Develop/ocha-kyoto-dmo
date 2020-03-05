3202996dcdbd669bf5b29ef9f9ae29f0db8815a4
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-05 11:21:55 +0900

    update

--- page-terms.php
+++ page-terms.php
@@ -2,16 +2,20 @@
 /*
 template name: Terms
 */
-
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header('low'); ?>
-
-<section class="sec lower-layer-wrap mt-0">
-<div class="wrap lower-layer pl-1 pr-1">
-<h2 class="page-ttl crimson"><?php echo get_the_title(); ?></h2>
+<?php
+if (have_posts()):
+while (have_posts()): the_post();
+?>
+<article class="sec article-main">
+<div class="wrap">
+<h2 class="article-ttl"><?php echo $t; ?></h2>
+<div class="post-main">
 <?php the_content(); ?>
 </div>
-</section>
-
+</div>
+</article>
+<?php endwhile; endif; ?>
 <?php get_footer();
